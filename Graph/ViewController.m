d1238de05906c1d53cd83a5e9d0533065b6a44dd
InnaLabynskaya <innalabynskaya@gmail.com>
2015-05-27 10:20:17 +0300

    New sorting algorithm

--- Graph/ViewController.m
+++ Graph/ViewController.m
@@ -52,108 +52,68 @@ static double const GenerationDistance = 100.0;
 
 - (void)updateNodesView
 {
+    NSArray *generations = [self arrayWithGenerations];
     [self.containerView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
-
-    Node *rootNode = self.graph.rootNode;
-    NSUInteger currentGenerationLevel = 0;
-    NSMutableSet *visitedNodes = [NSMutableSet set];
-    NSMutableArray *currentGeneration = [NSMutableArray array];
-    NSMutableSet *nextGeneration = [NSMutableSet set];
-    NSMutableArray *Generations = [NSMutableArray array];
-    [currentGeneration addObject:rootNode];
     double minRadius = 0;
-    
-    
-    [Generations addObject:currentGeneration];
-
-    for (NSMutableArray *generation in Generations) {
-    
-        [visitedNodes addObjectsFromArray:generation];
+    for (NSUInteger level = 0; level < generations.count; ++level) {
+        NSArray *generation = generations[level];
+        
         NSUInteger nodesCount = generation.count;
         NSUInteger circles = [self circlesForNodesCount:nodesCount withMinRadius:minRadius];
         double dAlpha = 1.95 * M_PI / nodesCount;
         double alpha = 0;
         NSUInteger currentCircle = 0;
         for(Node *node in generation) {
-            NodeView *nodeView = [self nodeViewForNode:node withGeneration:currentGenerationLevel];
+            NodeView *nodeView = [self nodeViewForNode:node withGeneration:level];
             double radius = minRadius + currentCircle * CircleDistance;
             nodeView.center = CGPointMake(radius * cos(alpha), radius * sin(alpha));
             currentCircle = (currentCircle + 1) % circles;
             alpha += dAlpha;
             [self.containerView addSubview:nodeView];
-             minRadius += circles * CircleDistance + GenerationDistance;
-//            [Generations addObjectsFromArray:children];
         }
-     
-        
-        for(Node*node in generation) {
-        NSArray *children = [self.graph firstGenerationFromNode:node];
-            [nextGeneration addObjectsFromArray:children];
-
-        [nextGeneration minusSet:visitedNodes];
-        [generation addObjectsFromArray:[nextGeneration allObjects]];
-        [nextGeneration removeAllObjects];
-        [Generations addObject:generation];
-        currentGenerationLevel++;
-        }
-        NSUInteger width = MAX(2*minRadius, self.view.frame.size.width);
-        NSUInteger height = MAX(2*minRadius, self.view.frame.size.height);
-        [self.containerView setFrame:CGRectMake(0, 0, width, height)];
-        [self.scrollView setContentSize:CGSizeMake(width, height)];
-        CGAffineTransform move = CGAffineTransformMakeTranslation(width/2, height/2);
-        for (NodeView *nodeView in self.containerView.subviews) {
-            [nodeView setTransform:move];
-        }
-
+        minRadius += circles * CircleDistance + GenerationDistance;
     }
     
+    NSUInteger width = MAX(2*minRadius, self.view.frame.size.width);
+    NSUInteger height = MAX(2*minRadius, self.view.frame.size.height);
+    [self.containerView setFrame:CGRectMake(0, 0, width, height)];
+    [self.scrollView setContentSize:CGSizeMake(width, height)];
+    CGAffineTransform move = CGAffineTransformMakeTranslation(width/2, height/2);
+    for (NodeView *nodeView in self.containerView.subviews) {
+        [nodeView setTransform:move];
+    }
+}
+
+- (NSArray*)arrayWithGenerations
+{
+    Node *rootNode = self.graph.rootNode;
+    NSMutableSet *visitedNodes = [NSMutableSet set];
+    NSMutableArray *currentGeneration = [NSMutableArray array];
+    NSMutableSet *nextGeneration = [NSMutableSet set];
+    NSMutableArray *Generations = [NSMutableArray array];
+    [currentGeneration addObject:rootNode];
+    [Generations addObject:currentGeneration];
     
-    
-    
-//    do {
-//        [visitedNodes addObjectsFromArray:currentGeneration];
-//
-//        NSUInteger nodesCount = currentGeneration.count;
-//        NSUInteger circles = [self circlesForNodesCount:nodesCount withMinRadius:minRadius];
-//        double dAlpha = 1.95 * M_PI / nodesCount;
-//        double alpha = 0;
-//        NSUInteger currentCircle = 0;
-//        for (Node *node in currentGeneration) {
-//            NodeView *nodeView = [self nodeViewForNode:node withGeneration:currentGenerationLevel];
-//            double radius = minRadius + currentCircle*CircleDistance;
-//            nodeView.center = CGPointMake(radius * cos(alpha), radius * sin(alpha));
-//            currentCircle = (currentCircle + 1) % circles;
-//            alpha += dAlpha;
-//            [self.containerView addSubview:nodeView];
-//        }
-//        minRadius += circles * CircleDistance + GenerationDistance;
-//        
-//        while (currentGeneration.count) {
-//            Node *node = currentGeneration.lastObject;
-//            [currentGeneration removeObject:node];
-//            NSArray *children = [self.graph firstGenerationFromNode:node];
-//            [nextGeneration addObjectsFromArray:children];
-//        }
-//
-//        [nextGeneration minusSet:visitedNodes];
-//        [currentGeneration addObjectsFromArray:[nextGeneration allObjects]];
-//        [nextGeneration removeAllObjects];
-//        currentGenerationLevel++;
-//    } while (currentGeneration.count);
-//    
-//    NSUInteger width = MAX(2*minRadius, self.view.frame.size.width);
-//    NSUInteger height = MAX(2*minRadius, self.view.frame.size.height);
-//    [self.containerView setFrame:CGRectMake(0, 0, width, height)];
-//    [self.scrollView setContentSize:CGSizeMake(width, height)];
-//    CGAffineTransform move = CGAffineTransformMakeTranslation(width/2, height/2);
-//    for (NodeView *nodeView in self.containerView.subviews) {
-//        [nodeView setTransform:move];
-//    }
+    for (NSUInteger i = 0; i < Generations.count; ++i) {
+        NSArray *generation = Generations[i];
+        [visitedNodes addObjectsFromArray:generation];
+        for (Node *node in generation) {
+            NSArray *children = [self.graph firstGenerationFromNode:node];
+            [nextGeneration addObjectsFromArray:children];
+        }
+        [nextGeneration minusSet:visitedNodes];
+        if (nextGeneration.count) {
+            [Generations addObject:[nextGeneration allObjects]];
+            [nextGeneration removeAllObjects];
+        }
+    }
+    return Generations;
 }
 
 - (NodeView*)nodeViewForNode:(Node*)node withGeneration:(NSUInteger)generation
 {
-    NodeView *nodeView = [[NodeView alloc] initWithFrame:CGRectMake(0, 0, NodeSize, NodeSize)];
+    NSUInteger nodeSize = NodeSize - MIN(25, 5*generation);
+    NodeView *nodeView = [[NodeView alloc] initWithFrame:CGRectMake(0, 0, nodeSize, nodeSize)];
     nodeView.node = node;
     nodeView.layer.cornerRadius = NodeSize/2;
     nodeView.layer.masksToBounds = YES;
